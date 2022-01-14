88938da7c79c8e0189813c35b2f874fc80fac557
Eduard Isaias <64340397+isaiasasenjo@users.noreply.github.com>
2022-01-14 07:00:54 +0100

    Create PetAuction
    Un contrato inteligente

---
+++ PetAuction.sol
@@ -0,0 +1,41 @@
+//SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract PetAuction {
+
+  Adoption[16] public adopters;
+
+  struct Adoption{
+   address owner;
+   uint256 date;
+   uint256 bid;
+  }
+
+  function bid(uint256 _petId, uint256 _bid) public {
+   require(_petId >= 0 && _petId <= 15);
+   require (block.timestamp < adopters[_petId].date + 3 minutes || adopters[_petId].date == 0 );
+   require (_bid > adopters[_petId].bid || adopters[_petId].bid == 0 );
+   adopters[_petId] = Adoption(msg.sender, block.timestamp, _bid);
+  }
+ }
+
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.6.0;
+
+contract PetAuction {
+
+Adoption[16] public adopters;
+
+ struct Adoption{
+   address owner;
+   uint256 date;
+   uint256 bid;
+ }
+
+ function bid(uint256 _petId, uint256 _bid) public {
+   require(_petId >= 0 && _petId <= 15);
+   require (now < adopters[_petId].date + 5 minutes || adopters[_petId].date == 0 );
+   require (_bid > adopters[_petId].bid || adopters[_petId].bid == 0 );
+   adopters[_petId] = Adoption(msg.sender, block.timestamp, _bid);
+ }
+}
