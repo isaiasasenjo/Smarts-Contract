2e7f7bb7e8912d2315e6fe7dd86fc96a5785c7ce
Eduard Isaias <64340397+isaiasasenjo@users.noreply.github.com>
2022-01-15 09:59:34 +0100

    Update Fabrication.sol

--- Fabrication.sol
+++ Fabrication.sol
@@ -2,9 +2,21 @@
 pragma solidity ^0.8.0;
 
 contract Fabrication {
+
 uint256 public units;
+address public owner;
+//el constructor solo se ejecuta una vez cuando se crea o despliega el contrato
+
+constructor(uint256 initialUnits){  
+    owner = msg.sender;
+    units = initialUnits;
+}
+
 
 function setUnits(uint256 newUnits) public {
-units = newUnits;
+    units = newUnits;
+    require(msg.sender == owner,"you are not");
+    //acciones
+    units = newUnits;
 }
 }
