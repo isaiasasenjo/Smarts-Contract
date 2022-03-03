981edca668426766f7c967afcaa99df0fb8d555d
Eduard Isaias <64340397+isaiasasenjo@users.noreply.github.com>
2022-03-03 17:36:06 +0100

    Update Greeter.sol

--- Greeter.sol
+++ Greeter.sol
@@ -1,17 +1,20 @@
 SPDX-License-Identifier: GPL-3.0
 pragma solidity ^0.4.0;
+
+//primer contrato
 contract mortal{
 	address owner;
 	// Inicializa el contrato con la variable owner
 	function mortal(){
         owner = msg.sender;
         }
-	// functio to recover the funds on the contract
+	// function to recover the funds on the contract
 	function kill(){
         if((msg == owner) selfdestruct(owner));
     }
 }
 
+// segundo contrato
 contract greeter is mortal {
 	string greeting;
 	
