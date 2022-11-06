bdc0e8cd1a8c527f819fe650df9b70ca84007659
Eduard Isaias <64340397+isaiasasenjo@users.noreply.github.com>
2022-11-06 18:54:01 +0100

    Update Contador.sol

--- Contador.sol
+++ Contador.sol
@@ -1,27 +1,22 @@
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.8.0;
+//SPDX-License-Identifier: GPL-3.0
 
+pragma solidity ^0.8.0;
 contract Contador {
 	uint256 count;
-
-	function Contador(uint256 _count){
+	string letra; 
+	function Contador1(uint256 _count) public{
 		count = _count;
 	}
-
-
 	function setCount(uint256 _count) public {
 		count = _count;
 	}
-
 	function incrementCount() public {
 		count = count+1;
 	}
-
 	function getCount() public view returns(uint256){
-		count = _count;
+		return count;
 	}
-
-	function getNumber() public view returns(uint256) {
+	function getNumber() public pure returns(uint256) {
 		return 200;
 	}
 }   // cierra el contrato Contador
